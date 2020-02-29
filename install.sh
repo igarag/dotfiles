753b953709ad4519e09f511d545bf6b0e3202b75
nachoaz <n.arranz.agueda@gmail.com>
2020-02-29 20:05:11 +0100

    curl added into install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # Installs many programs that I use in my daily 
 # life as well as the configuration of programs such as vim, bash, etc.
 
-
+sudo apt install curl
 sudo apt install -y vim-gnome
 
 
