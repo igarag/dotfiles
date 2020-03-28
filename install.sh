5ac9c67c64934baf2f7c3f0e38722c902542c5bb
NachoAz <n.arranz.agueda@gmail.com>
2020-03-28 18:25:17 +0100

    installation improved

--- install.sh
+++ install.sh
@@ -1,19 +1,43 @@
 # Installs many programs that I use in my daily 
-# life as well as the configuration of programs such as vim, bash, etc.
+# life as well as the configuration of programs such as vim, ranger, etc.
+
+HOME=$( getent passwd "$USER" | cut -d: -f6 )
+RANGER_PATH=$HOME/.config/ranger
+TERMINATOR_PATH=$HOME/.config/terminator
 
 sudo apt install curl
 sudo apt install -y vim-gnome
 
-
+### bashrc
 mv ~/.bashrc ~/.bashrc.bak
 cp bashrc ~/.bashrc
 
+### vimrc
 mv ~/.vimrc ~/.vimrc.bak
 cp vimrc ~/.vimrc
 
-## Install virtualenvs
+### ranger
+if [ -d "$RANGER_PATH" ];
+then
+    sudo apt install ranger
+fi
+cp rc.conf $RANGER_PATH/
 
+# terminator
+if [ -d "$TERMINATOR_PATH" ];
+then
+    sudo apt install terminator
+fi
+mv $TERMINATOR_PATH/config $TERMINATOR_PATH/config.bak
+cp config $TERMINATOR_PATH/
+
+
+## Install virtualenvs
 sudo apt-get install build-essential libssl-dev libffi-dev python-dev
 sudo add-apt-repository ppa:linuxuprising/apps
+
+
+
+
 
 
