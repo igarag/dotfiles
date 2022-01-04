a9645760a3961752887d835ccdccee5351260c55
igarag <n.arranz.agueda@gmail.com>
2022-01-04 23:27:08 +0100

    :recycle: Cheatsheet project

---
+++ devhints/main.py
@@ -0,0 +1,18 @@
+from rich.console import Console
+from rich.table import Column, Table
+
+console = Console()
+
+table = Table(show_header=True, header_style="bold ")
+table.add_column("Shortcut")
+table.add_column("Description", justify="left")
+table.add_column("Example", justify="left")
+
+from cheatsheets.vim import cheatsheets
+
+for cheatsheet in cheatsheets:
+    print(cheatsheet)
+    table.add_row(cheatsheet[0], cheatsheet[1], cheatsheet[2])
+
+console.print(table)
+
