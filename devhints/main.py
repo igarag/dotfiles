26b6222ca62b867adfa55fd2260dbd5107f3b04c
igarag <n.arranz.agueda@gmail.com>
2022-01-08 23:32:37 +0100

    :sparkles: New main program

--- devhints/main.py
+++ devhints/main.py
@@ -3,18 +3,28 @@ import click
 from rich.console import Console
 from rich.table import Column, Table
 
-console = Console()
+def create_table() -> Table:
+    table = Table(show_header=True, header_style="bold ")
+    table.add_column("Shortcut")
+    table.add_column("Description", justify="left")
+    table.add_column("Example", justify="left")
 
-table = Table(show_header=True, header_style="bold ")
-table.add_column("Shortcut")
-table.add_column("Description", justify="left")
-table.add_column("Example", justify="left")
+    return table    
 
-from cheatsheets.vim import cheatsheets
+@click.command("--tool", default="vim")
+def main(tool):
+    console = Console()
 
-for cheatsheet in cheatsheets:
-    table.add_row(cheatsheet[0], cheatsheet[1], cheatsheet[2])
-
-console.print(table)
+    create_table()
 
 
+    from cheatsheets.vim import cheatsheets
+
+    for cheatsheet in cheatsheets:
+        table.add_row(cheatsheet[0], cheatsheet[1], cheatsheet[2])
+
+    console.print(table)
+
+
+if __name__ == "__main__":
+    main()
