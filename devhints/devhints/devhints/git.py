4925488af68c4c2559522774b843c3c7c07da386
igarag <n.arranz.agueda@gmail.com>
2022-01-09 23:50:00 +0100

    :sparkles: New project structure

---
+++ devhints/devhints/devhints/git.py
@@ -0,0 +1,17 @@
+cheatsheets = [
+    [
+        "commit",
+        "commit changes",
+        "git commit -m 'message'",
+    ],
+    [
+        "status",
+        "Check status of working directory",
+        "git status",
+    ],
+    [
+        "checkout",
+        "Change to a different branch",
+        "git checkout branch",
+    ],
+]
