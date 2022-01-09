aab28e031d0cec7a1753a1cfb7c76f0d3c07b852
igarag <n.arranz.agueda@gmail.com>
2022-01-09 23:50:00 +0100

    :sparkles: New project structure

---
+++ devhints/devhints/devhints/docker.py
@@ -0,0 +1,18 @@
+
+cheatsheets = [
+    [
+        "ps",
+        "Check the current docker containers",
+        "docker ps",
+    ],
+    [
+        "images",
+        "Check all the downloaded images",
+        "docker images",
+    ],
+    [
+        "stop",
+        "Stop the container given a <ID>",
+        "docker stop <ID>",
+    ],
+]
