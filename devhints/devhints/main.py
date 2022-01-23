b0f818ad657520621b1bade90f46fed8ad04cfa8
igarag <n.arranz.agueda@gmail.com>
2022-01-23 23:04:11 +0100

    :recycle: New project structure in devhints

--- devhints/devhints/main.py
+++ devhints/devhints/main.py
@@ -16,15 +16,16 @@ def create_table() -> Table:
 def print_devhint(table: Table, tool: str) -> None:
     console = Console()
 
-    cheatsheet_module = importlib.import_module(f"devhints.devhints.{tool}")
+    cheatsheet_module = importlib.import_module(f"cheatsheets.{tool}")
 
     for cheatsheet in cheatsheet_module.cheatsheets:
         table.add_row(cheatsheet[0], cheatsheet[1], cheatsheet[2])
     console.print(table)
 
 
+@click.argument("tool")
 @click.command()
-@click.option("--tool")
+@click.option("--tool", type=str)
 def main(tool):
 
     table = create_table()
