bc50461d7c78c7a8284fac37de68b6edbaac3cc4
nachoaz <nachoaz.dev@gmail.com>
2022-11-24 22:39:05 +0100

    :wrench: replace specific path to $HOME env variable

--- config/fzf/fzf.zsh
+++ config/fzf/fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/nachoaz/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/nachoaz/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/nachoaz/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/nachoaz/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
