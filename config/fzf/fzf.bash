838ac65d3475bf56219dabc7c18c30476599c097
nachoaz <nachoaz.dev@gmail.com>
2023-04-02 17:52:50 +0200

    :refactor: Remove personal absolute path

--- config/fzf/fzf.bash
+++ config/fzf/fzf.bash
@@ -1,13 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/nachoaz/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/nachoaz/.fzf/bin"
+# if [[ ! "$PATH" == */home/iarranz/.fzf/bin* ]]; then
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/nachoaz/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/nachoaz/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
